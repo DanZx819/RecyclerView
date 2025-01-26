b3a48d1ce2665da9ba8e807e27f9f7588d999936
DanZx819 <147755543+DanZx819@users.noreply.github.com>
2025-01-26 04:17:09 -0300

    Recycler view v1

---
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -0,0 +1,53 @@
+package com.devspace.recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+// Adapatação entre o dataclass e o item_list
+
+class ContactListAdapter: ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils()) {
+
+    //Criar um view holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+        return ContactViewHolder(view)
+    }
+    // bind - atrelar o dado com a UI views
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        val contact = getItem(position)
+        holder.bind(contact)
+    }
+
+    // view holder = view que segura os dados
+    class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        private val tvName = view.findViewById<TextView>(R.id.tv_name)
+        private val tvNumber = view.findViewById<TextView>(R.id.tv_number)
+        private val image_view = view.findViewById<ImageView>(R.id.img_vw)
+
+        fun bind(contact: Contact){
+            tvName.text = contact.name
+            tvNumber.text = contact.number
+            image_view.setImageResource(contact.icon)
+        }
+    }
+    // compara a diferença quando a nossa lista é atualizada
+    class  ContactDiffUtils : DiffUtil.ItemCallback<Contact>(){
+        override fun areItemsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: Contact, newItem: Contact): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/devspace/recyclerview/MainActivity.kt
+++ app/src/main/java/com/devspace/recyclerview/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +18,118 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        //Criar um data class
+        //Criar lista para o tipo do data class
+        //Criar um adapter
+        //Setar o adapter
+        //Linear layout manager
+        //Submeter a Lista
+
+
+        val rvlist = findViewById<RecyclerView>(R.id.rv_list)
+        val adapter = ContactListAdapter()
+
+        rvlist.adapter = adapter
+        rvlist.layoutManager = LinearLayoutManager(this)
+        adapter.submitList(contacts)
     }
 }
+
+private val contacts = listOf(
+    Contact(
+        "Carla",
+        "(55+) 18 997234585",
+        R.drawable.sample1
+    ),
+
+    Contact(
+        "Guilherme",
+        "(55+) 18 994780385",
+        R.drawable.sample2
+    ),
+
+    Contact(
+        "Manuela",
+        "(55+) 18 996230402",
+        R.drawable.sample3
+    ),
+
+    Contact(
+        "Olivia",
+        "(55+) 18 995728743",
+        R.drawable.sample4
+    ),
+    Contact(
+        "Kamila",
+        "(55+) 18 992033453",
+        R.drawable.sample5
+    ),
+
+    Contact(
+        "Pietra",
+        "(55+) 18 923345678",
+        R.drawable.sample6
+    ),
+
+    Contact(
+        "Sofia",
+        "(55+) 18 992304567",
+        R.drawable.sample7
+    ),
+
+    Contact(
+        "Francisco",
+        "(55+) 18 992034855",
+        R.drawable.sample8
+    ),
+
+    Contact(
+        "Marcos",
+        "(55+) 18 997543723",
+        R.drawable.sample9
+    ),
+
+    Contact(
+        "Leandro",
+        "(55+) 18 997020403",
+        R.drawable.sample10
+    ),
+
+    Contact(
+        "Julia",
+        "(55+) 18 994070892",
+        R.drawable.sample11
+    ),
+
+    Contact(
+        "Amir",
+        "(55+) 18 994030504",
+        R.drawable.sample12
+    ),
+
+    Contact(
+        "Paula",
+        "(55+) 18 997436532",
+        R.drawable.sample13
+    ),
+
+    Contact(
+        "Anderson",
+        "(55+) 18 997030506",
+        R.drawable.sample14
+    ),
+
+    Contact(
+        "Joana",
+        "(55+) 18 997040385",
+        R.drawable.sample15
+    ),
+
+    Contact(
+        "Carol",
+        "(55+) 18 996421234",
+        R.drawable.sample16
+    )
+)
